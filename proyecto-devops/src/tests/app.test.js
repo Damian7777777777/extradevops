54b3b2dbf820011aa5d6d0a541e2d142235d057f
Damian7777777777 <damiantego@gmail.com>
2026-04-25 11:02:17 -0600

    x

--- proyecto-devops/src/tests/app.test.js
+++ proyecto-devops/src/tests/app.test.js
@@ -55,7 +55,7 @@ describe('Tasks Routes', () => {
     expect(res.body).toHaveProperty('id');
   });
 
-  test('GET /health - debe responder okso', async () => {
+  test('GET /health - debe responder jijijia', async () => {
     const res = await request(app).get('/health');
     expect(res.statusCode).toBe(200);
     expect(res.body.status).toBe('oksote');
